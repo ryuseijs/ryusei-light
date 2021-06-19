50b183caebc0e0b50d126885b9031d0ff50a968c
NaotoshiFujita <fujita.naotoshi@gmail.com>
2021-06-19 19:57:18 +0900

    Change the way to export categories.

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -3,7 +3,7 @@ export { RyuseiLight as default } from './core/RyuseiLight/RyuseiLight';
 export { Lexer } from './core/Lexer/Lexer';
 export * from './components';
 export * from './languages';
+export * from './constants/categories';
 export * as components from './components';
 export * as languages from './languages';
-export * as CATEGORIES from './constants/categories';
 //# sourceMappingURL=index.d.ts.map
--- src/js/index.ts
+++ src/js/index.ts
@@ -3,12 +3,10 @@ export { RyuseiLight as default } from './core/RyuseiLight/RyuseiLight';
 export { Lexer } from './core/Lexer/Lexer';
 export * from './components';
 export * from './languages';
+export * from './constants/categories';
 
 // Exports all components as a single object
 export * as components from './components';
 
 // Exports all languages as a single object
 export * as languages from './languages';
-
-// Exports all categories
-export * as CATEGORIES from './constants/categories';
